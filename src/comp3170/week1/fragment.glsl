7b114e42c85439d1f31e0ccec7ce89057c14cfa0
sarthdan <140594774+sarthdan@users.noreply.github.com>
2025-03-03 15:18:45 +1100

    fragment ahanged

--- src/comp3170/week1/fragment.glsl
+++ src/comp3170/week1/fragment.glsl
@@ -7,7 +7,9 @@ layout(location = 0) out vec4 o_colour;	// output to colour buffer
 
 void main() {
    vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)
-   float d = distance(p, vec2(0.5, 0.5));     // calculate distance to midpoint
+   vec2 v = Maths.abs(p - vec2(0.5, 0.5));
+   float d = max(v.x, v.y); 
+   //float d = distance(p, vec2(0.5, 0.5));     // calculate distance to midpoint
   
    if (d < 0.5) {
       o_colour = vec4(u_colour, 1);
